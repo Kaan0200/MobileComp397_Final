fd0d6d9e68ed46af1b642aef930cb92393d22598
Kaan Atesoglu <aka43@case.edu>
2015-11-23 13:47:15 -0500

    Added Android Project skeleton

---
+++ app/src/main/java/com/example/kaan/vocomaster3000/MachineActivity.java
@@ -0,0 +1,13 @@
+package com.example.kaan.vocomaster3000;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MachineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine);
+    }
+}
